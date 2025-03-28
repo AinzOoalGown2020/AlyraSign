7f5a90ccd4f4210759eaaec76d6801d2de80dc0b
AinzOoalGown2020 <yannick@jesson.biz>
2025-03-28 11:14:43 +0100

    fix: correction des erreurs de déploiement Vercel

--- next.config.js
+++ next.config.js
@@ -1,27 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-      crypto: false,
-      stream: false,
-      path: false,
-      os: false,
-      zlib: false,
-      http: false,
-      https: false,
-      buffer: false,
-      process: false,
-    };
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+        stream: false,
+        path: false,
+        os: false,
+        zlib: false,
+        http: false,
+        https: false,
+        buffer: false,
+        process: false,
+      };
+    }
     return config;
   },
   output: 'standalone',
   experimental: {
-    serverComponentsExternalPackages: ['@coral-xyz/anchor'],
+    serverComponentsExternalPackages: ['@coral-xyz/anchor', 'bn.js', 'buffer'],
   },
 }
 
